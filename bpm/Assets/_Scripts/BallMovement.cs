d0b3726421fa8eeda2c73f6fa294658d1b103441
John Skoufoulas <giannarasgr97@gmail.com>
2022-12-22 20:58:39 +0200

    Bholes

--- bpm/Assets/_Scripts/BallMovement.cs
+++ bpm/Assets/_Scripts/BallMovement.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour {
 
-    public float maxJumpStrength = 2000;
-    public float minJumpStrength = 1000;
+    public float maxJumpStrength = 1000;
+    public float minJumpStrength = 500;
+    public float step = 350;
     public float speed = 3f;
+    public float maxVelocity = 10f;
     public bool isGrounded;
+
+    public Vector3 velocity;
 
     private float jumpStrength;
     private Rigidbody rigid;
@@ -19,35 +23,58 @@ public class BallMovement : MonoBehaviour {
         rigid = gameObject.GetComponent<Rigidbody>();
         groundCheck = gameObject.GetComponent<SphereCollider>();
         jumpStrength = minJumpStrength;
+        //rigid.drag = 1f;
+
+        velocity = Vector3.zero;
+
+        //Physics.gravity = new Vector3(0,-10.0f,0);
     }
 
     private void Update() {
         //Movement on ground
+        Vector3 horizontalForce = Vector3.zero;
+        Vector3 verticalForce = Vector3.zero;
         if (Input.GetAxis("Horizontal") > 0) {
-            rigid.AddForce(Vector3.right * speed);
+            //rigid.AddForce(Vector3.right * speed);
+            horizontalForce = Vector3.right;
         }
         else if (Input.GetAxis("Horizontal") < 0) {
-            rigid.AddForce(-Vector3.right * speed);
-        }
-        else {
-
+            //rigid.AddForce(-Vector3.right * speed);
+            horizontalForce = -Vector3.right;
         }
 
         if (Input.GetAxis("Vertical") > 0) {
-            rigid.AddForce(Vector3.forward * speed);
+            //rigid.AddForce(Vector3.forward * speed);
+            verticalForce = Vector3.forward;
         }
         else if (Input.GetAxis("Vertical") < 0) {
-            rigid.AddForce(-Vector3.forward * speed);
+            //rigid.AddForce(-Vector3.forward * speed);
+            verticalForce = -Vector3.forward;
         }
+
+        if (Mathf.Abs(rigid.velocity.x) > maxVelocity) horizontalForce = Vector3.zero;
+        if (Mathf.Abs(rigid.velocity.z) > maxVelocity) verticalForce = Vector3.zero;
+
+        rigid.AddForce(Vector3.Normalize(horizontalForce + verticalForce) * speed);
+
 
         if (Input.GetKey(KeyCode.Space)) {
-            jumpStrength = Mathf.Min(jumpStrength + Time.deltaTime * 2000, maxJumpStrength);
+            jumpStrength = Mathf.Min(jumpStrength + Time.deltaTime * step, maxJumpStrength);
         }
 
-        if (Input.GetKeyUp(KeyCode.Space) && isGrounded) {
+        if (Input.GetKeyUp(KeyCode.Space) && isGrounded){
             rigid.AddForce(Vector3.up * jumpStrength);
             jumpStrength = minJumpStrength;
         }
+
+        if (!isGrounded) rigid.AddForce(Vector3.down * 2);
+
+        if (!Input.anyKey) {
+            rigid.velocity = new Vector3(rigid.velocity.x * .99f, rigid.velocity.y , rigid.velocity.z * .99f);
+            if (Mathf.Abs(rigid.velocity.x) < 0.01f) rigid.velocity = new Vector3(0, rigid.velocity.y, rigid.velocity.z);
+            if (Mathf.Abs(rigid.velocity.z) < 0.01f) rigid.velocity = new Vector3(rigid.velocity.x, rigid.velocity.y, 0);
+        }
+        velocity = rigid.velocity;
     }
 
     private void OnCollisionEnter(Collision collision) {

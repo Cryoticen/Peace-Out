b8e9c8ca4c00e9a5ec62724afd0aec1ddc323f9c
George Sachinoglou <george.sachinoglou@gmail.com>
2023-01-01 22:48:14 +0200

    Added ambient sounds to specific spots, removed jump
    Yes jump is stupid, breaks immersion because you can jump continuously and sometimes bugs the camera - therefore its gone.

---
+++ Peace out/Assets/_Scripts/RandomSoundsScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundsScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public AudioClip audio;
+    bool hasplayed = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!hasplayed) { 
+                AudioSource.PlayClipAtPoint(audio, transform.position);
+                hasplayed = true;
+            }
+            
+            
+        }
+    }
+
+
+}
